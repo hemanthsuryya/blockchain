73b58cea0fd8d5af7913cf2f14e7305cb6f57317
hemanthsuryya <hemanthsurya10500@gmail.com>
2021-04-22 16:45:26 +0530

    P2P

--- app/index.js
+++ app/index.js
@@ -1,14 +1,15 @@
 const { response } = require('express');
 const express       = require('express');
 const Blockchain    = require('../blockchain/');
+const P2pServer     = require('./p2p-server');
 
 const bodyParser = require('body-parser');
-
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app   = express();
 app.use(bodyParser.json());
 const bc    = new Blockchain();
+const p2pServer = new P2pServer(bc);
 
 app.get('/blocks', (req, res) => {
     res.json(bc.chain);
@@ -23,3 +24,4 @@ app.post('/mine', (req, res) => {
 });
 
 app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`));
+p2pServer.listen();
